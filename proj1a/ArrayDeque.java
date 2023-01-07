6eda4d417dc120e0b73e32c79b65c60eb1344994
jackyliu <18922251299@163.com>
2023-01-08 00:56:18 +0800

    update(LinkedListDeque): finish basic framework

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,12 @@
+
+/*
+ * @File:   ArrayDeque.java
+ * @Desc:
+ * @Author: jacky
+ * @Repo:   https://github.com/jackyliu16
+ * @Date:   2023/1/8 上午12:19
+ * @Version:0.0
+ */
+
+public class ArrayDeque {
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,27 @@
+
+/*
+ * @File:   LinkedListDeque.java
+ * @Desc:
+ * @Author: jacky
+ * @Repo:   https://github.com/jackyliu16
+ * @Date:   2023/1/8 上午12:19
+ * @Version:0.0
+ */
+
+public class LinkedListDeque<T> {
+    LinkedListDeque () {  }
+
+    public void addFirst(T item) { }
+    public void addLast(T item) {  }
+    public boolean isEmpty() { return true; }
+    public int size() { return 0; }
+    public void printDeque() {  }
+    public T removeFirst() { return null; }
+    public T removeLast() { return null; }
+    public T get(int index) { return null; }
+
+}
+
+class DequeNode<T> {
+
+}
