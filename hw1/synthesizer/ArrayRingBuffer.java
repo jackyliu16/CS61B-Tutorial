a2cd161470395b24db00eab6582da0c3958b6632
jackyliu <18922251299@163.com>
2023-01-12 14:16:43 +0800

    FIX(hw1): using other's file to check if the problem occur in ArrayRingBuffer.java

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,16 +1,10 @@
 package synthesizer;// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
 
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 
-//TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
 public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
-    private int first;            // index for the next dequeue or peek
+    private int first;
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
@@ -20,31 +14,19 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements Bound
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        this.first = 0;
-        this.last = 0;
-        this.fillCount = 0;
+        rb = (T[]) new Object[capacity];
         this.capacity = capacity;
-        this.rb = (T[]) new Object[capacity+1];
-//        Arrays.fill(this.rb, 0);
+        first = last = fillCount = 0;
     }
 
     /**
-     * Adds x to the end of the ring buffer. If there is no room, then
-     * throw new RuntimeException("Ring buffer overflow"). Exceptions
-     * covered Monday.
+     * Adds x to the end of the ring buffer. If there is no room, then throw new
+     * RuntimeException("Ring buffer overflow"). Exceptions covered Monday.
      */
     public void enqueue(T x) {
         if (isFull()) {
-            throw new RuntimeException("Ring Buffer Overflow");
+            throw new RuntimeException("Ring buffer overflow");
         }
-        // if now last wasn't empty then will not enqueue
-//        if (this.rb[this.last] != null) {
-//            // if the place have been use
-//            return;
-//        } else {
-//            this.rb[this.last] = x;
-//            this.last = (this.last + 1 + this.capacity) % this.capacity;
-//        }
         rb[last++] = x;
         if (last == capacity) {
             last = 0;
@@ -53,23 +35,18 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements Bound
     }
 
     /**
-     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
-     * throw new RuntimeException("Ring buffer underflow"). Exceptions
-     * covered Monday.
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then throw
+     * new RuntimeException("Ring buffer underflow"). Exceptions covered Monday.
      */
     public T dequeue() {
         if (isEmpty()) {
-            throw new RuntimeException("Ring Buffer Underflow");
+            throw new RuntimeException("Ring buffer underflow");
         }
-        this.fillCount--;
-//        T res = this.rb[this.first];
-//        this.rb[this.first] = null;
-//        this.first = (this.first + 1 + this.capacity) % this.capacity;
-//        return res;
         T res = rb[first++];
         if (first == capacity) {
             first = 0;
         }
+        fillCount--;
         return res;
     }
 
@@ -78,52 +55,40 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements Bound
      */
     public T peek() {
         if (isEmpty()) {
-            throw new RuntimeException("Ring Buffer Underflow");
+            throw new RuntimeException("Ring buffer underflow");
         }
-        assert this.fillCount != 0;
-        return this.rb[this.first];
+        return rb[first];
     }
 
-    // TODO: When you get to part 5, implement the needed code to support iteration.
-    public String toString() {
-        StringBuilder sb = new StringBuilder("(");
-        if (isFull()) {
-            sb.append(String.format("%s, ", this.rb[this.first].toString()));
-            for (int i = this.first + 1; i != this.last; i = (i + 1 + this.capacity) % this.capacity) {
-                sb.append(String.format("%s, ", this.rb[i].toString()));
-            }
-        }
-        for (int i = this.first; i != this.last; i = (i + 1 + this.capacity) % this.capacity) {
-            sb.append(String.format("%s, ", this.rb[i].toString()));
-        }
-        return sb.toString() + ")";
-    }
-
-    @Override
+    /** returns an iterator */
     public Iterator<T> iterator() {
-        return new ArrayRingBufferIter();
+        return new BufferIterator();
     }
 
-    private class ArrayRingBufferIter implements Iterator<T> {
+    private class BufferIterator implements Iterator<T> {
         private int pos;
-        private int curNum;
+        private int num;
 
-        public ArrayRingBufferIter() {
+        BufferIterator() {
             pos = first;
-            curNum = 0;
+            num = 0;
         }
 
         @Override
         public boolean hasNext() {
-            return curNum < fillCount;
+            return num < fillCount;
         }
 
         @Override
         public T next() {
-            T res = rb[pos];
-            pos = (pos + 1 + capacity) % capacity;
-            curNum++;
-            return res;
+            T returnItem = rb[pos];
+            pos++;
+            if (pos == capacity) {
+                pos = 0;
+            }
+            num++;
+            return returnItem;
         }
+
     }
 }
